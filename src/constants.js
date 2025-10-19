16f4499bd7ea4e81c2127285bcc9888776971315
Zaidzyy <thezaiduniversegames@gmail.com>
2025-10-20 02:16:19 +0400

    Add constants folder to fix Vercel build

---
+++ src/constants.js
@@ -0,0 +1,44 @@
+
+export const services = [
+    {
+        title: "Ethical Hacking",
+        icon: 'https://imgs.search.brave.com/dUSA_LQl0L7ycpFkrL-Z1srQzf6A5DxrgsHOtHNJSUA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDExMzc5/Nzk2LmpwZw',
+    },
+    {
+        title: "Cloud Security",
+        icon: 'https://imgs.search.brave.com/J4MJjatC3dO9KxMMZQpzP7J179BwvolUN92H0p-RaTU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9jbG91ZC1jb21w/dXRpbmctY3liZXIt/c2VjdXJpdHlfMzQ2/MjktOTM4LmpwZz9z/ZW10PWFpc19oeWJy/aWQmdz03NDAmcT04/MA',
+    },
+    {
+        title: "Python",
+        icon: 'https://imgs.search.brave.com/fkfZXpHc-Gzhvy4pmwNK11fIcBTl8Ot4VDy-UOWXNuQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS12ZWN0/b3IvZnV0dXJpc3Rp/Yy1hYnN0cmFjdC1i/YWNrZ3JvdW5kXzE5/MzA2Ni05NDQuanBn/P3NlbXQ9YWlzX2h5/YnJpZCZ3PTc0MCZx/PTgw',
+    },
+];
+
+export const name = 'Md Zaid';
+
+export const experiences = [
+    {
+        'company': 'Flamingus Technologies',
+        'role': 'IT Consultant Intern',
+        'duration': 'June 2024 - August 2024',
+        'logo': 'https://imgs.search.brave.com/DCGCCRx6hXXZ_ErsP6s9oWetP3MxD-Kb8X7IJ2c8grs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zMy5h/bWF6b25hd3MuY29t/L21lZGlhLm1peHJh/bmsuY29tL3Byb2Zp/bGVwaWMvYjM2OWFk/ZmQ5OGFlN2JmNGNl/YzRhYjQ3NDdjNGFj/Zjg',
+        'points': [
+            'Served as a team leader for a training module and Led internal team discussions on security best practices, including researching and presenting on Multiple domains to upskill the team.',
+            'Gained essential security infrastructure experience through hands-on Experience on core systems like Active Directory and by configuring Azure Virtual Machines (VMs) for secure deployments ⚙️.',
+            'Contributed to the end-to-end development of a company website/portal, actively applying development skills and influencing project outcomes 📱.'
+        ],
+        'url': 'https://www.flamingus.com/',
+    },
+    {
+        'company': 'Microsoft Tech Club (MTC) and ACM-W',
+        'role': '1st Place at Intra-University Hackathon',
+        'duration': 'March 2025',
+        'logo': 'https://imgs.search.brave.com/8dKF6oaPcUp2caQK-XLH89pqiUHQ2k6vWbm9czfrY08/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS12ZWN0/b3IvZ29sZC10cm9w/aHktY2FydG9vbi12/ZWN0b3ItaWxsdXN0/cmF0aW9uXzEwNjI4/NTctMjkxNy5qcGc_/c2VtdD1haXNfaW5j/b21pbmcmdz03NDAm/cT04MA',
+        'points': [
+            'This project focused on developing an AI-driven chatbot using Botpress for optimizing medication tracking, demonstrating my ability to apply AI skills to solve a complex, real-world challenge in the healthcare sector.',
+        ],
+        'url': 'https://www.linkedin.com/posts/mdzaid2005_bitspilani-acmwxmtc-mtc-activity-7304126544405237760-K6KU?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD4pXY0BsisUrb-yyS4U1GRQQk_h0xIrve8',
+    },
+]
+
+
